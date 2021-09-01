c802591ce04ec42f15bbd4a6df27dad1199bb383
lyswifter <ly70835@163.com>
2021-09-01 18:59:00 +0800

    fix code

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"io"
 	"log"
-	"os"
-	"strings"
 	"sync"
-	"time"
 )
 
 const repoPath = "~/.punkcenter"
@@ -22,59 +17,59 @@ func main() {
 
 	DataStores()
 
-	go func() {
-		AllIps = readline("ips")
-
-		ticker := time.NewTicker(30 * time.Second)
-
-		for {
-			select {
-			case <-ticker.C:
-				var notReceived []string
-				for _, ip := range AllIps {
-					isHas := false
-					for _, r := range received {
-						if ip == r {
-							isHas = true
-							break
-						}
-					}
-
-					if isHas {
-						continue
-					}
-
-					notReceived = append(notReceived, ip)
-				}
-				log.Printf("notReceived(%d) ips: %v", len(notReceived), notReceived)
-			}
-		}
-	}()
-
 	SetupServers()
+
+	// go func() {
+	// 	AllIps = readline("ips")
+
+	// 	ticker := time.NewTicker(30 * time.Second)
+
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			var notReceived []string
+	// 			for _, ip := range AllIps {
+	// 				isHas := false
+	// 				for _, r := range received {
+	// 					if ip == r {
+	// 						isHas = true
+	// 						break
+	// 					}
+	// 				}
+
+	// 				if isHas {
+	// 					continue
+	// 				}
+
+	// 				notReceived = append(notReceived, ip)
+	// 			}
+	// 			log.Printf("notReceived(%d) ips: %v", len(notReceived), notReceived)
+	// 		}
+	// 	}
+	// }()
 }
 
-func readline(path string) []string {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil
-	}
-	defer f.Close()
+// func readline(path string) []string {
+// 	f, err := os.Open(path)
+// 	if err != nil {
+// 		return nil
+// 	}
+// 	defer f.Close()
 
-	rd := bufio.NewReader(f)
+// 	rd := bufio.NewReader(f)
 
-	var ret = []string{}
-	for {
-		line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
+// 	var ret = []string{}
+// 	for {
+// 		line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
 
-		if err != nil || io.EOF == err {
-			break
-		}
+// 		if err != nil || io.EOF == err {
+// 			break
+// 		}
 
-		line = strings.Replace(line, "\n", "", -1)
+// 		line = strings.Replace(line, "\n", "", -1)
 
-		ret = append(ret, line)
-	}
+// 		ret = append(ret, line)
+// 	}
 
-	return ret
-}
+// 	return ret
+// }
